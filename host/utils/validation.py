6b4fc10e393db105524c9942caa49b5a0ffbd92c
michaelhobbs <michael@hobbs.im>
2021-03-24 19:10:50 -0300

    Code formatting and documentation

--- host/utils/validation.py
+++ host/utils/validation.py
@@ -8,6 +8,7 @@ import utils.messages as msg
 from utils.config import COMMAND_MAP
 import re
 
+
 def _validate_args(message, command):
     '''
     Verifies for a given command that all its required args are present and are in the correct format.
@@ -18,13 +19,18 @@ def _validate_args(message, command):
     else:
         args_valid = True
         for arg in command['args']:
-            args_valid = args_valid and arg in message and re.match(command['args'][arg], str(message[arg]))
+            args_valid = args_valid and arg in message and re.match(
+                command['args'][arg], str(message[arg]))
         return args_valid
 
-# Validate message matches application's interface
+
 def validate_message(message):
-    isValid = 'cmd' in message and message['cmd'] in COMMAND_MAP and _validate_args(message, COMMAND_MAP[message['cmd']])
-    if not isValid:    
+    '''
+    Validate message matches application's interface
+    '''
+    isValid = 'cmd' in message and message['cmd'] in COMMAND_MAP and _validate_args(
+        message, COMMAND_MAP[message['cmd']])
+    if not isValid:
         msg.send_message({'msgType': 'log', 'msg': "Error: invalid command"})
         msg.send_message({'msgType': 'result', 'success': False})
     return isValid
